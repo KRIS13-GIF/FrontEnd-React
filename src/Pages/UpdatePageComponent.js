1d42db13f97d321d689a3e1c598d0e597227fdd1
KRIS13-GIF <bekakris@yahoo.com>
2023-07-14 16:00:34 +0200

    updating features for CRUD

--- src/Pages/UpdatePageComponent.js
+++ src/Pages/UpdatePageComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import NavbarK from "../Components/Navbar";
@@ -8,6 +8,7 @@ import axios from "axios";
 function UpdatePost() {
   const params = useParams();
   const navigate=useNavigate();
+  const[data, setData]=useState({});
 
 
   const [formData, setFormData] = useState({
@@ -15,6 +16,27 @@ function UpdatePost() {
     description:"",
    
   });
+
+
+  useEffect(()=>{
+    getData();
+  }, [])
+
+
+  const getData=()=>{
+      axios.get(`http://localhost:5002/api/program/postId/${params.id}`)
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+        console.log("data: ", response.data);
+    })
+    .catch((error) => {
+        console.error('Error fetching posts:', error);
+    });
+};
+
+
+
 
   const update = () => {
     console.log(formData)
@@ -33,32 +55,35 @@ function UpdatePost() {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-    
- 
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value === '' ? ' ' : value,
+    }));
   };
 
   return (
     <>
+
       <div>
         <NavbarK />
       </div>
       <div style={{ marginLeft: "50px", marginRight: "50px", marginTop: "50px" }}>
+        { data.status =='PENDING' && 
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" name="title" onChange={handleInputChange} />
+            <Form.Control type="text" name="title" onChange={handleInputChange} value={formData.title || data.title || ''}></Form.Control>
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" name="description" onChange={handleInputChange} />
+            <Form.Control type="text" name="description" onChange={handleInputChange} value={formData.description || data.description || ''} />
           </Form.Group>
           <Button variant="primary" onClick={update}>
             Submit
           </Button>
-        </Form>
+        </Form>}
       </div>
     </>
   );
