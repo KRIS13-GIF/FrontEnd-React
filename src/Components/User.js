93e13e0d52606c200f003872f2aaea34d7a3004e
KRIS13-GIF <bekakris@yahoo.com>
2023-07-17 22:16:53 +0200

    softDelete for users

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,10 +1,26 @@
 import Card from 'react-bootstrap/Card';
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 function User({user}){
     const navigate = useNavigate();
+
+
+    function softDelete(){
+
+      axios.put(`http://localhost:5002/api/program/softDelete/${user.id}`)
+      .then(response => {
+        console.log(response);
+        alert('Soft delete done !')
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Error creating user:', error);
+      });
+    }
+
     return (
         
         <Card style={{ width: '18rem' }}>
@@ -15,8 +31,9 @@ function User({user}){
             <Card.Text>
                 {user.address}
             </Card.Text>
-           <Card.Link onClick={()=>navigate(`fav/${user.id}`)}>Favorites</Card.Link> 
-            <Card.Link onClick={() => navigate(`post/${user.id}`)}>Posts</Card.Link>
+           <Card.Link style={{cursor:'pointer'}} onClick={()=>navigate(`fav/${user.id}`)}>Favorites</Card.Link> 
+            <Card.Link style={{cursor:'pointer'}} onClick={() => navigate(`post/${user.id}`)}>Posts</Card.Link>
+            <Card.Link style={{cursor:'pointer'}} onClick={()=>softDelete()}>Delete</Card.Link>
             
           </Card.Body>
         </Card>
